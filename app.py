dd40545985a93fd25fb8716f3701188ffab15dfd
Kriss Watt <kriss.watt@gmail.com>
2012-07-16 14:42:45 +0100

    Project created.

---
+++ app.py
@@ -0,0 +1,81 @@
+import requests
+import json
+import operator
+from redis import StrictRedis
+from flask import Flask, make_response
+
+
+DISCUSSION_API = 'http://discussionapi.guardian.co.uk/discussion-api/'
+CONTENT_API = 'http://content.guardianapis.com/'
+RESPONSE_CACHE_TIME = 30 # in seconds
+
+app = Flask(__name__)
+redis = StrictRedis('localhost', 6379)
+
+
+@app.route('/comments/latest/<userid>')
+def latest_comments(userid):
+    """Return JSON of recent comment counts by section"""
+    cached_data = redis.get('da/users/{}'.format(userid))
+    if cached_data:
+        response = make_response(cached_data)
+    else:
+        data = get_user_comments(userid)
+        response = make_response(data)
+    response.mimetype = 'application/json'
+    return response
+
+
+def get_user_comments(userid):
+    """Build a map of recent comment counts by section"""
+    r = requests.get(
+        '{}profile/{}/comments?pageSize=500'.format(DISCUSSION_API, userid)
+    )
+
+    # If call failed, swallow the error and return no data
+    if r.status_code != 200:
+        return json.dumps({})
+
+    # Map article keys to number of comments
+    comments = {}
+    for comment in json.loads(r.content)['comments']:
+        key = comment['discussion']['key']
+        if not key in comments:
+            comments[key] = 0
+        comments[key] = comments[key] + 1
+
+    # Sort by comment count
+    comments = sorted(
+        comments.iteritems(), key=operator.itemgetter(1), reverse=True
+    )
+
+    # Map comments to sections
+    key_sections = redis.hgetall('da/key-sections')
+    for key, count in comments:
+        if not key in key_sections.keys():
+            r = requests.get('{}{}'.format(CONTENT_API, key[1:]))
+            section = json.loads(r.content)['response']['content']['sectionId']
+            redis.hset('da/key-sections', key, section)
+            key_sections[key] = section
+
+    # Map comments to sections
+    comments_sections = {}
+    for key, count in comments:
+        section = key_sections[key]
+        if not section in comments_sections:
+            comments_sections[section] = 0
+        comments_sections[section] = comments_sections[section] + count
+
+    # Cache response in redis
+    rkey = 'da/users/{}'.format(userid)
+    data = json.dumps(comments_sections)
+    pipe = redis.pipeline()
+    pipe.set(rkey, data)
+    pipe.expire(rkey, RESPONSE_CACHE_TIME)
+    pipe.execute()
+
+    return data
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
